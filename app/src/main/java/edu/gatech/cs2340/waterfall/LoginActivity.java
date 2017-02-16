943cbed6063e3be10c0ee178f504f9e28b39db90
Sean Sain <seanrsain@gmail.com>
2017-02-16 01:18:01 -0500

    add navigation controller

--- app/src/main/java/edu/gatech/cs2340/waterfall/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/waterfall/LoginActivity.java
@@ -16,9 +16,13 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    private FirebaseAuth auth;
+public class LoginActivity extends AppCompatActivity {
+    private static FirebaseAuth auth;
     private static final int RC_SIGN_IN = 0;
+
+    public static FirebaseAuth getAuth() {
+        return auth;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
             Log.d("LOGGED", auth.getCurrentUser().getEmail());
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
         } else {
             startActivityForResult(AuthUI.getInstance()
                             .createSignInIntentBuilder()
@@ -35,24 +42,23 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             .build(),
                     RC_SIGN_IN);
         }
-        findViewById(R.id.logout_btn).setOnClickListener(this);
     }
 
-    public void onClick(View v) {
-        if (v.getId() == R.id.logout_btn) {
-            AuthUI.getInstance()
-                    .signOut(this)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        public void onComplete(@NonNull Task<Void> task) {
-                            // user is now signed out
-                            Log.d("AUTH", "Logged out");
-                            Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
-                            startActivity(intent);
-                            finish();
-                        }
-                    });
-        }
-    }
+//    public void onClick(View v) {
+//        if (v.getId() == R.id.logout_btn) {
+//            AuthUI.getInstance()
+//                    .signOut(this)
+//                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                        public void onComplete(@NonNull Task<Void> task) {
+//                            // user is now signed out
+//                            Log.d("AUTH", "Logged out");
+//                            Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
+//                            startActivity(intent);
+//                            finish();
+//                        }
+//                    });
+//        }
+//    }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
@@ -62,6 +68,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             // Successfully signed in
             if (resultCode == ResultCodes.OK) {
                 Log.d("AUTH", auth.getCurrentUser().getEmail());
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         }
     }
