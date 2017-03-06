d414b55d9b43861088f84c3fb3cf1203b7a372e1
Mehul Mohagaonkar <memochamp96@gmail.com>
2017-03-06 01:07:07 -0500

    Updated source and purity report

--- app/src/main/java/edu/gatech/cs2340/waterfall/model/PurityReport.java
+++ app/src/main/java/edu/gatech/cs2340/waterfall/model/PurityReport.java
@@ -3,17 +3,35 @@ package edu.gatech.cs2340.waterfall.model;
 import android.location.Location;
 import android.os.Parcel;
 
+import com.google.firebase.database.DatabaseReference;
+
+import edu.gatech.cs2340.waterfall.controller.WelcomeActivity;
+
 /**
  * Created by seanrsain on 3/6/17.
  */
 
 public class PurityReport extends Report {
-    public PurityReport(User user, Location location) {
+
+    private overallCondition OverallCondition;
+    private int virusPPM;
+    private int containmentPPM;
+
+    public PurityReport(User user, Location location, overallCondition OC, int virus, int containment) {
         super(user, location);
+        OverallCondition = OC;
+        virusPPM = virus;
+        containmentPPM = containment;
     }
 
     public void writeToDatabase() {
-
+        DatabaseReference mReportDatabase = WelcomeActivity.getmReportDatabase();
+        mReportDatabase.child(getReportNo()).child("dateAndTime").setValue(getDate());
+        mReportDatabase.child(getReportNo()).child("reporterName").setValue(getUserName());
+        mReportDatabase.child(getReportNo()).child("location").setValue(getLocation());
+        mReportDatabase.child(getReportNo()).child("overallCondition").setValue(OverallCondition);
+        mReportDatabase.child(getReportNo()).child("virus").setValue(virusPPM);
+        mReportDatabase.child(getReportNo()).child("containment").setValue(containmentPPM);
     }
 
     @Override
@@ -23,6 +41,11 @@ public class PurityReport extends Report {
 
     @Override
     public void writeToParcel(Parcel parcel, int i) {
-
+        parcel.writeString(this.getDate().toString());
+        parcel.writeString(this.getUserName());
+        parcel.writeString(this.getLocation().toString());
+        parcel.writeString(this.OverallCondition.toString());
+        parcel.writeInt(this.virusPPM);
+        parcel.writeInt(this.containmentPPM);
     }
 }
--- app/src/main/java/edu/gatech/cs2340/waterfall/model/SourceReport.java
+++ app/src/main/java/edu/gatech/cs2340/waterfall/model/SourceReport.java
@@ -3,17 +3,49 @@ package edu.gatech.cs2340.waterfall.model;
 import android.location.Location;
 import android.os.Parcel;
 
+import com.google.firebase.database.DatabaseReference;
+
+import edu.gatech.cs2340.waterfall.controller.WelcomeActivity;
+
 /**
  * Created by seanrsain on 3/6/17.
  */
 
 public class SourceReport extends Report{
 
-    public SourceReport(User user, Location location) {
+    public waterType getWaterType() {
+        return WaterType;
+    }
+
+    public void setWaterType(waterType waterType) {
+        WaterType = waterType;
+    }
+
+    public waterCondition getWaterCondition() {
+        return WaterCondition;
+    }
+
+    public void setWaterCondition(waterCondition waterCondition) {
+        WaterCondition = waterCondition;
+    }
+
+    private waterType WaterType;
+    private waterCondition WaterCondition;
+
+    public SourceReport(User user, Location location, waterType WT, waterCondition WC) {
         super(user, location);
+        WaterType = WT;
+        WaterCondition = WC;
     }
 
     public void writeToDatabase() {
+        DatabaseReference mReportDatabase = WelcomeActivity.getmReportDatabase();
+        mReportDatabase.child(getReportNo()).child("dateAndTime").setValue(getDate());
+        mReportDatabase.child(getReportNo()).child("reporterName").setValue(getUserName());
+        mReportDatabase.child(getReportNo()).child("location").setValue(getLocation());
+        mReportDatabase.child(getReportNo()).child("type").setValue(WaterType);
+        mReportDatabase.child(getReportNo()).child("condition").setValue(WaterCondition);
+        mReportDatabase.child(getReportNo()).child("user").setValue(getUser());
 
     }
 
@@ -24,7 +56,11 @@ public class SourceReport extends Report{
 
     @Override
     public void writeToParcel(Parcel parcel, int i) {
-
+        parcel.writeString(this.getDate().toString());
+        parcel.writeString(this.getUserName());
+        parcel.writeString(this.getLocation().toString());
+        parcel.writeString(this.WaterType.toString());
+        parcel.writeString(this.WaterCondition.toString());
     }
 
 }
