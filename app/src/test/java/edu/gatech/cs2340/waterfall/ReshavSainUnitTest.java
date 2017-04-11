2a9cf5cadea2f598d394df3ad3acc7752213ac95
Sean Sain <seanrsain@gmail.com>
2017-04-10 21:50:14 -0400

    adding more tests

--- app/src/test/java/edu/gatech/cs2340/waterfall/ReshavSainUnitTest.java
+++ app/src/test/java/edu/gatech/cs2340/waterfall/ReshavSainUnitTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import edu.gatech.cs2340.waterfall.model.Model;
 import edu.gatech.cs2340.waterfall.model.User;
+import edu.gatech.cs2340.waterfall.model.Worker;
 
 import static org.junit.Assert.*;
 
@@ -31,12 +32,17 @@ public class ReshavSainUnitTest {
     public void UserEqualsTest() throws Exception {
         String type = "user";
         String uid = "34344";
+        String uid1 = "34335";
         String name = "Foo Bar";
         String email = "foo@bar.com";
         int zip = 30332;
         String phoneNumber = "909900900";
         User newUser = new User(uid, name, email, zip, phoneNumber);
         User newUser1 = new User(uid, name, email, zip, phoneNumber);
+        User newUser2 = new User(uid1, name, email, zip, phoneNumber);
+        Worker worker = new Worker(uid1, name, email, zip, phoneNumber);
         assertTrue(newUser.equals(newUser1));
+        assertFalse(newUser1.equals(newUser2));
+        assertFalse(newUser2.equals(worker));
     }
 }
